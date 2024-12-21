0e8ce4efc775febad22170eb64431011bbe1b239
Charlie871001 <mahui871001@gmail.com>
2024-12-21 15:01:26 +0800

    multiSignature

--- contracts/multiSignature/multiSignatureClient.sol
+++ contracts/multiSignature/multiSignatureClient.sol
@@ -2,10 +2,40 @@
 pragma solidity ^0.8.20;
 
 interface IMultiSignature {
-    function getValidatorCount(
+    /**
+     * @dev 
+     * @param msgHash dev hash of the message  
+     * @param lastIndex     last index of the signature array
+     */
+    function getValidSignature(
         bytes32 msgHash,
         uint256 lastIndex
     ) external view returns (uint256);
+}
+
+contract multiSignatureClient {
+    uint256 private constant multiSignaturePosition = uint256(keccak256("multiSignature")));
+    uint256 private constant defaultIndex = 0;
+
+    /**
+     * @dev 
+     * @param multiSignature address of the multiSignature contract
+     */
+    constructor(address multiSignature) public {
+        require(multiSignature != address(0), "multiSignature address cannot be zero");
+        saveValue(multiSignaturePosition, uint256(multiSignature));
+    }
+
+    /**
+     * @dev 
+     * @param position 
+     * @param value 
+     */
+    function saveValue(uint256 position, uint256 value) internal {
+        assembly {
+            sstore(position, value)
+        }
+    }
 }
 
 
