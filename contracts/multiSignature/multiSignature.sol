734a17d5cfc9072b269aa9d2851efd091e51318d
Charlie871001 <mahui871001@gmail.com>
2024-12-22 18:29:45 +0800

    multiSignature

--- contracts/multiSignature/multiSignature.sol
+++ contracts/multiSignature/multiSignature.sol
@@ -7,23 +7,54 @@ library whiteListAddress {
      * @param whiteList array of addresses
      * @param temp address to add
      */
-    function addWhiteListAddress(address[] storage whiteList,address temp) internal{
-        if(!isEligibleAddress(whiteList,temp)){
+    function addWhiteListAddress(
+        address[] storage whiteList,
+        address temp
+    ) internal {
+        if (!isEligibleAddress(whiteList, temp)) {
             whiteList.push(temp);
         }
     }
 
     /**
-     * @dev 
-     * @param _whiteList array of addresses
-     * @param temp  address to check
+     * @dev remove address from whiteList
+     * @param whiteList array of addresses
+     * @param temp remove address
      */
-    function isEligibleAddress(address[] memory _whiteList,address temp) internal pure returns(bool){
-        for(uint i=0;i<_whiteList.length;i++){
-            if(temp==_whiteList[i]){
+    function removeWhiteListAddress(
+        address[] storage whiteList,
+        address temp
+    ) internal returns (bool) {
+        uint256 len = whiteList.length;
+        uint256 i = 0;
+        for (; i < len; i++) {
+            if (temp == whiteList[i]) {
+                whiteList[i] = whiteList[len - 1];
+                whiteList.pop();
                 return true;
             }
         }
         return false;
     }
+
+    /**
+     * @dev
+     * @param _whiteList array of addresses
+     * @param temp  address to check
+     */
+    function isEligibleAddress(
+        address[] memory _whiteList,
+        address temp
+    ) internal pure returns (bool) {
+        for (uint i = 0; i < _whiteList.length; i++) {
+            if (temp == _whiteList[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+contract multiSignature is multiSignatureClient {
+
 }
