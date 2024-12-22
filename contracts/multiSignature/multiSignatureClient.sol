5d2b0d0572c8d9d36230622e02b520b7ee2ba01b
Charlie871001 <mahui871001@gmail.com>
2024-12-22 17:23:21 +0800

    multiSignatureClient

--- contracts/multiSignature/multiSignatureClient.sol
+++ contracts/multiSignature/multiSignatureClient.sol
@@ -14,7 +14,7 @@ interface IMultiSignature {
 }
 
 contract multiSignatureClient {
-    uint256 private constant multiSignaturePosition = uint256(keccak256("multiSignature")));
+    uint256 private constant multiSignaturePosition = uint256(keccak256("org.multiSignature.storge")));
     uint256 private constant defaultIndex = 0;
 
     /**
@@ -28,6 +28,38 @@ contract multiSignatureClient {
 
     /**
      * @dev 
+     */
+    function getMultiSignatureAddress() public view returns(address){
+        return address(getValue(multiSignaturePosition)));
+    }
+
+    modifier validCall {
+        checkMultiSignature();
+        _;
+    }
+
+    /**
+     * @dev check if the transaction is approved by the multiSignature contract
+     */
+    function checkMultiSignature() internal {
+        address multiSignature = getMultiSignatureAddress();
+        bytes32 msgHash = keccak256(abi.encodePacked(msg.sender, address(this)));
+        uint256 newIndex = IMultiSignature(multiSignature).getValidSignature(msgHash, defaultIndex);
+        require(newIndex > defaultIndex, "multiSignatureClient: This tx is not approved");
+    }
+
+    /**
+     * @dev get value from storage
+     * @param position 
+     */
+    function getValue(uint256 position) internal view returns(uint256 value){
+        assembly {
+            value := sload(position)
+        }
+    }
+
+    /**
+     * @dev save value to storage
      * @param position 
      * @param value 
      */
